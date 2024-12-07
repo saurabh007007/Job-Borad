481879f36679d39924f35aa85a51333fa558573e
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2024-12-07 15:33:27 +0530

    no work with vps

--- FRONTEND/src/utils/constant.js
+++ FRONTEND/src/utils/constant.js
@@ -1,18 +1,18 @@
-// export const USER_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/user";
-// export const JOB_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/job";
-// export const APPLICATION_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/application";
-// export const COMPANY_API_END_POINT =
-//   "https://job-borad-3ose.onrender.com/api/v1/company";
-
-export const USER_API_END_POINT = "http://206.189.138.106:8080/api/v1/user";
-export const JOB_API_END_POINT = "http://206.189.138.106:8080/api/v1/job";
+export const USER_API_END_POINT =
+  "https://job-borad-3ose.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-borad-3ose.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://206.189.138.106:8080/api/v1/application";
+  "https://job-borad-3ose.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "http://206.189.138.106:8080/api/v1/company";
+  "https://job-borad-3ose.onrender.com/api/v1/company";
+
+// export const USER_API_END_POINT = "http://206.189.138.106:8080/api/v1/user";
+// export const JOB_API_END_POINT = "http://206.189.138.106:8080/api/v1/job";
+// export const APPLICATION_API_END_POINT =
+//   "http://206.189.138.106:8080/api/v1/application";
+// export const COMPANY_API_END_POINT =
+//   "http://206.189.138.106:8080/api/v1/company";
 
 // export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
 // export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
