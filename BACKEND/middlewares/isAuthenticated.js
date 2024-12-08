3f41e7c907497fb4f48e08cb1bbe3dc15b8d329a
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2024-12-08 18:23:50 +0530

    h

--- BACKEND/middlewares/isAuthenticated.js
+++ BACKEND/middlewares/isAuthenticated.js
@@ -2,14 +2,17 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
   try {
+    console.log("middleware auth req.cookies", req.cookies);
     const token = req.cookies.token;
-    console.log(token);
+    console.log("middleware auth token", token);
     if (!token) {
+      console.log("middleware auth token line 12", token);
       return res.status(401).json({
         message: "User not authenticated",
         success: false,
       });
     }
+    console.log("middleware auth token line 14", token);
     const decode = await jwt.verify(token, process.env.SECRET_KEY);
     if (!decode) {
       return res.status(401).json({
