3308328666a4ef9f5f590642ffcdbade88fbbb6e
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2024-12-31 23:23:54 +0530

    railway

--- FRONTEND/src/utils/constant.js
+++ FRONTEND/src/utils/constant.js
@@ -28,3 +28,12 @@ export const COMPANY_API_END_POINT =
 //   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/application";
 // export const COMPANY_API_END_POINT =
 //   "https://backend-b4oz7n10s-saurabh007007s-projects.vercel.app/api/v1/company";
+
+export const USER_API_END_POINT =
+  "https://job-borad-production.up.railway.app/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-borad-production.up.railway.app/api/v1/job";
+export const APPLICATION_API_END_POINT =
+  "https://job-borad-production.up.railway.app/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://job-borad-production.up.railway.app/api/v1/company";
